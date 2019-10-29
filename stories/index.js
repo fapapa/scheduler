ddbedd10e7e66f90200c08cc6364371d56c495b4
Fabio Papa <fabtheman@gmail.com>
2019-10-28 18:34:41 -0700

    Add interviewer list item component

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
 const days = [
   {
@@ -26,6 +27,12 @@ const days = [
     spots: 0,
   }
 ];
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
 
 storiesOf("Button", module)
   .addParameters({
@@ -63,4 +70,32 @@ storiesOf("DayList", module)
   ))
   .add("Tuesday", () => (
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+      <InterviewerListItem
+    id={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+      />
+  ))
+  .add("Selected", () => (
+      <InterviewerListItem
+    id={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected
+      />
+  ))
+  .add("Clickable", () => (
+      <InterviewerListItem
+    id={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    setInterviewer={action("setInterviewer")}
+      />
   ));
