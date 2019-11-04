c4e27a6cc433b8c8bda8f00d427438621ba866b8
Fabio Papa <fabtheman@gmail.com>
2019-11-04 13:03:59 -0800

    Add a restriction to the props of interviewer list

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import InterviewerListItem from "components/InterviewerListItem";
 
 import "components/InterviewerListItem.scss";
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers.map((interviewer) => (
+  const interviewers = props.interviewers.map(interviewer => (
     <InterviewerListItem
       key={interviewer.id}
       name={interviewer.name}
@@ -16,11 +17,14 @@ export default function InterviewerList(props) {
   ));
 
   return (
-      <section className="interviewers">
+    <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-      {interviewers}
-    </ul>
-      </section>
-  )
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
 }
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
