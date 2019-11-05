a9a874b1fed93a30f22b1e98eccb9b0c3714464c
Fabio Papa <fabtheman@gmail.com>
2019-11-04 17:46:11 -0800

    Switch to async/await in application tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -12,13 +12,12 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
-    return waitForElement(() => getByText("Monday")).then(res => {
-      fireEvent.click(getByText("Tuesday"));
+    await waitForElement(() => getByText("Monday"));
 
-      expect(getByText(/leopold silvers/i)).toBeInTheDocument();
-    });
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText(/leopold silvers/i)).toBeInTheDocument();
   });
 });
