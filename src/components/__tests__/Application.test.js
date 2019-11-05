9470fb753510f68aff8b495e89cf6fac1cea781d
Fabio Papa <fabtheman@gmail.com>
2019-11-04 17:01:10 -0800

    Add a test specifying that a user can select a day

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  waitForElement,
+  fireEvent
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(res => {
+      fireEvent.click(getByText("Tuesday"));
+
+      expect(getByText(/leopold silvers/i)).toBeInTheDocument();
+    });
+  });
 });
